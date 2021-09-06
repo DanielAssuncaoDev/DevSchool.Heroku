52f7457689587815f2354a4b4f731264fba8f3e1
DanielAssuncaoDev <danielaristotelis@gmail.com>
2021-09-06 11:46:41 -0300

    API programada

--- src/index.js
+++ src/index.js
@@ -1,12 +1,104 @@
-import db from './db';
+import db from './db.js';
 import express from 'express';
 import cors from 'cors'
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
 
 
+
+
+// LISTAR ALUNOS MATRICULADOS
+
+app.get('/matriculas', async (req, resp) => {
+    try {
+        let r = await db.tb_matricula.findAll();
+        resp.send(r);
+
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+
+
+
+// INSERIR MATRICULA    
+
+app.post('/matriculas', async (req, resp) => {
+    try {
+        let m = req.body;
+
+        let inserirMatrucula = {
+            nm_aluno: m.nome,
+            nr_chamada: m.chamada,
+            nm_curso: m.curso,
+            nm_turma: m.turma 
+        };
+
+        let r = await db.tb_matricula.create(inserirMatrucula);
+        resp.sendStatus(200);
+
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+
+
+
+
+// ALTERA UMA MATRICULA
+
+app.put('/matriculas/:id', async (req, resp) => {
+    try {
+        let id = req.params.id;
+        let m = req.body;
+
+        let matricula = await db.tb_matricula.update(
+            {
+                nm_aluno: m.nome,
+                nr_chamada: m.chamada,
+                nm_curso: m.curso,
+                nm_turma: m.turma 
+            },
+            {
+                where: {
+                    id_matricula: id
+                   }
+            }
+        );
+        resp.sendStatus(200)
+
+    } catch (e) {
+        resp.send({erro: e.toString()});
+    }
+})
+
+
+
+
+app.delete('/matriculas/:id', async (req, resp) => {
+    try {
+        let id = req.params.id;
+
+        let r = await db.tb_matricula.destroy({
+            where: {id_matricula: id}
+        })
+
+        resp.sendStatus(200)
+
+    } catch (e) {
+        resp.send({erro: e.toString()})
+    }
+})
+
+
+
+
+
 app.listen( process.env.PORT,
                     x => console.log(`Server up at PORT:${process.env.PORT}`) )
+
+
